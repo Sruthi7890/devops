1014a6772c98aaf1555352b3d42fadbe55b94996
sruthi7890 <kethavarapuharshitha@gmail.com>
2025-01-05 20:25:38 +0530

    commiting the file

---
+++ Loops.java
@@ -0,0 +1,9 @@
+public class Loops
+{
+    public static void main(String[] args) {
+        for(int n = 0; n < 5; n = n + 1)
+        {
+            System.out.println("Sruthi");
+        }
+    }
+}
